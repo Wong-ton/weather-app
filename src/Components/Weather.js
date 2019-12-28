8e2568fa202ce8d2a94164287ba59183491ba39c
Jason Wong <jwong175@hotmail.com>
2019-12-27 16:52:26 -0800

    Add conversion formulas for temperature and timezones

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
 import '../App.css';
 import WeatherInfo from './WeatherInfo';
+import moment from 'moment';
 
 function Weather() {
     const apiKey = 'ecf07367ad26b3ab3716f250f847bab6';
     const [searchQuery, setSearchQuery] = useState('');
     const [weatherData, setWeatherData] = useState({
         city: null,
+        country: null,
         temp: null,
         humidity: null,
         desc: null,
+        sunrise: null,
+        sunset: null,
+        timeDiff: null
     });
 
     function updateSearchQuery(e) {
@@ -19,17 +24,29 @@ function Weather() {
     function getWeather() {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&appid=${apiKey}`)
         .then(response => response.json())
-        // .then(data => console.log(data))
         .then(data => setWeatherData({
             city: data.name,
-            temp: kelvinToFahrenheit(data.main.temp),
+            country: data.sys.country,
+            tempF: kelvinToFahrenheit(data.main.temp),
+            tempC: kelvinToCelsius(data.main.temp),
             humidity: data.main.humidity,
-            desc: data.weather[0].description
+            desc: data.weather[0].description,
+            timeDiff: data.timezone,
+            sunrise: timezoneAdjust(data.sys.sunrise, data.timezone),
+            sunset: timezoneAdjust(data.sys.sunset, data.timezone),
         }))
     }
 
     function kelvinToFahrenheit(kelvin) {
         return ((kelvin - 273.15) * (9.0 / 5.0) + 32).toFixed(0);
+    }
+
+    function kelvinToCelsius(kelvin) {
+        return (kelvin - 273.15).toFixed(0);
+    }
+
+    function timezoneAdjust(unix, timeDiff) {
+        return moment.utc(new Date(unix * 1000 + (timeDiff * 1000)).toUTCString()).format("LT")
     }
 
     return(
@@ -38,7 +55,7 @@ function Weather() {
             <section>
                 <input 
                 className="search-input"
-                placeholder="Enter City or ZIP"
+                placeholder="Enter City"
                 onChange={updateSearchQuery}
                 />
                 <button onClick={getWeather}>Search</button>
--- src/Components/WeatherInfo.js
+++ src/Components/WeatherInfo.js
@@ -2,21 +2,25 @@ import React from 'react';
 import '../App.css';
 
 function WeatherInfo(props) {
-    const { city, temp, humidity, desc} = props.data;
+    const { city, country, tempF, tempC, humidity, desc, sunrise, sunset, timeDiff} = props.data;
 
     return(
         <React.Fragment>
             <div>
-              <p>City: {city}</p>
+              <p>Location: {city}, {country}</p>
             </div>
             <div>
               <p>Description: {desc}</p>
             </div>
             <div>
-              <p>Temperature: {temp}</p>
+              <p>Temperature: {tempF} &deg;F  /  {tempC} &deg;C</p>
             </div>
             <div>
-              <p>Humidity: {humidity}</p>
+              <p>Humidity: {humidity}%</p>
+            </div>
+            <div>
+              <p>Sunrise: {sunrise}</p>
+              <p>Sunset: {sunset}</p>
             </div>
         </React.Fragment> 
 
