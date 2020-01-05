1bacf4bd5ac49b8e26204e53e1f9c159b729cb4e
Jason Wong <jwong175@hotmail.com>
2020-01-05 14:43:56 -0800

    Updates

---
+++ precache-manifest.2463d515d3818d0a54c3b8c55c6ad519.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4c24a7d9e5ba3ec80d7430ee0a8a5d",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "30636ec0bd7b5cbc86b5",
+    "url": "/weather-app/static/css/main.c1606ead.chunk.css"
+  },
+  {
+    "revision": "6f19d03f281c0685115b",
+    "url": "/weather-app/static/js/2.edd7a514.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather-app/static/js/2.edd7a514.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30636ec0bd7b5cbc86b5",
+    "url": "/weather-app/static/js/main.ccb5167c.chunk.js"
+  },
+  {
+    "revision": "df1ac26dc0e7b7d18b20",
+    "url": "/weather-app/static/js/runtime-main.a7aa11b4.js"
+  }
+]);
